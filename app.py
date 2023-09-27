57bf38e3414f589dc093be8183672cf4605ac3ce
Oscar Pindaro <oscar.pindaro@intellico.ai>
2023-09-27 12:45:46 +0000

    created app for plotting

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import io
 from typing import List
 import gradio as gr
+from matplotlib.colors import Normalize
+import numpy as np
 from sentence_transformers import SentenceTransformer
 from tqdm.auto import trange, tqdm
 import chromadb
@@ -7,6 +10,11 @@ import pandas as pd
 import torch
 import time
 from transformers import pipeline
+from PIL import Image
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 # tweets = [
@@ -49,8 +57,15 @@ sentence_embedder_id = "sentence-transformers/all-MiniLM-L6-v2"
 similarity_type = "cosine"
 
 
+global_topic_list : List[str]= []
+returned_tweets : List[str]= []
+returned_tweets_emb : List[np.array] = []
 
 MAX_TEXT_BOXES = 100
+USE_STANDARD_INTERFACE = False
+PCA_DIMENSION = 10
+TSNE_DIMENSION = 2
+N_TWEETS = 2000
 
 
 # instantiate model
@@ -82,7 +97,6 @@ def variable_outputs(k):
     k = int(k)
     return [gr.Textbox.update(visible=True)]*k + [gr.Textbox.update(visible=False)]*(MAX_TEXT_BOXES-k)
 
-
 def parse_topics(topic_string: str):
     """Converts the string given by the client into a list of topics.
     Topics are divided with commands. Leading and Trailing spaces are removed
@@ -96,7 +110,6 @@ def parse_topics(topic_string: str):
     topics_list = [topic.strip() for topic in topic_string.split(",")]
     return topics_list
 
-    
 
 def sentiment_analysis(tweets : List[str]):
     global sentiment_classifier
@@ -105,31 +118,43 @@ def sentiment_analysis(tweets : List[str]):
 def search(query, k, min_len, topics_string) -> List[str]:
     global collection
     global embedder_model
+    global global_topic_list
+    global returned_tweets
+    global returned_tweets_emb
+    
     k= int(k)
     print("Querying Database...")
     start = time.time()
     response = collection.query(
         query_embeddings=embedder_model.encode(query).tolist(),
-        n_results=k,
+        n_results=N_TWEETS,
         where={
         "lenght":{
             "$gte":int(min_len)
-        }
+        },        
     } 
     )
     print(f"Execution Time:{(time.time()-start):.2f}\n")
     # sentiments analysis
     tweets = response["documents"][0]
     tweets_embs = response["embeddings"]
+    # update global variables for plot
+    returned_tweets = tweets[:]
+    returned_tweets_emb = tweets_embs
+    # reduce to k the lists
+    tweets = tweets[0:k]
+    
     print("Analysing Sentiment...")
     start = time.time()
     sentiments = sentiment_analysis(tweets)
     print(f"Execution Time:{(time.time()-start):.2f}\n")
     # topic modeling
+    topic_query_result = None
     if topics_string is None or topics_string != "":
         print("Topic Modeling")
         start = time.time()
         topics : List[str] = parse_topics(topics_string)
+        global_topic_list = topics
         # create a db for that
         TOPIC_COLLECTION_NAME = "Topics"
         ram_chroma_client = chromadb.Client()
@@ -155,36 +180,142 @@ def search(query, k, min_len, topics_string) -> List[str]:
         else:
             sentiment["emoji"] = "😰"
     #topic output
-    topic_query_result = [f"#{topic[0]}" for topic in topic_query_result]
-    output = [f"{tweets[i]}\nSENTIMENT - {sentiments[i]['emoji']} with score {sentiments[i]['score']:.2f}\n"
-              f"{topic_query_result[i]}" for i in range(len(tweets))]
+    if topic_query_result is None:
+        output = [
+            f"{tweets[i]}\nSENTIMENT - {sentiments[i]['emoji']} with score {sentiments[i]['score']:.2f}"
+            for i in range(len(tweets))
+            ]
+    else:
+        topic_query_result = [f"#{topic[0]}" for topic in topic_query_result]
+        output = [f"{tweets[i]}\nSENTIMENT - {sentiments[i]['emoji']} with score {sentiments[i]['score']:.2f}\n"
+                f"{topic_query_result[i]}" for i in range(len(tweets))]
+
+
+
+
+
+    
     return "\n\n".join(output)
     # return response["documents"][0]
 
+def generate_image(topic):
+    global global_topic_list
+    global embedder_model
+    global returned_tweets
+    global returned_tweets_emb
     
-textboxes = []  
-for i in range(MAX_TEXT_BOXES):
-    t = gr.Textbox(f"Tweet {i}")
-    textboxes.append(t)
+    if not topic in global_topic_list:
+        print("Warning! given topic not in list!!!")
+
+    encoded_topic =  embedder_model.encode(topic)
+    embedded_tweets = embedder_model.encode(returned_tweets)
+
+    def cosine_similarity(vecA, vecB):
+        dot_product = np.dot(vecA, vecB)
+        normA = np.linalg.norm(vecA)
+        normB = np.linalg.norm(vecB)
+        return dot_product / (normA * normB)
+
+    similarities = cosine_similarity(encoded_topic, np.transpose(embedded_tweets))
+    pca = PCA(n_components=PCA_DIMENSION)
+    data_pca = pca.fit_transform(embedded_tweets)
+    # Apply t-SNE for visualization
+    data_tsne = TSNE(n_components=TSNE_DIMENSION).fit_transform(data_pca)
+    # Plot with Seaborn
+    # Plot with Seaborn
+    fig, ax = plt.subplots(figsize=(10, 8))
+
+    # Using a scatter plot from matplotlib to have a continuous color bar
+    norm = Normalize(vmin=min(similarities), vmax=max(similarities))
+    sc = ax.scatter(data_tsne[:, 0], data_tsne[:, 1], c=similarities, cmap="viridis", norm=norm)
+
+    ax.set_title("t-SNE visualization colored by similarity to the first point")
+
+    # Display color bar
+    cbar = plt.colorbar(sc, ax=ax)
+    cbar.set_label('Similarity', rotation=270, labelpad=15)
+
+    # Convert plot to PIL Image
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    image = Image.open(buf)
+    image.load()
+    buf.close()
+
+    
+    return image
 
 
-n_tweet_component =  gr.Number(default=20, label="Number of Tweets")
-minimum_lenght = gr.Number(default=0, label="Minimum number of characters")
+if USE_STANDARD_INTERFACE:
+    n_tweet_component =  gr.Number(value=20, default=20, label="Number of Tweets")
+    minimum_lenght = gr.Number(value=120, default=120, label="Minimum number of characters")
+    demo = gr.Interface(
+        fn=search,
+        inputs=[
+            gr.Textbox(lines=1, placeholder="Keywords"),
+            n_tweet_component,
+            minimum_lenght,
+            gr.Textbox(lines=1, value="Rock, Metal, EDM, Dance, Heavy Metal, Techno, Funky, Grunge, Pop, Kpop, Rap"),
+        ],
+        outputs=[gr.Textbox(placeholder="List of Tweets"), "image"],
+        live=False,  # Disable live updates
+        title="Grapho Search",
+        description="Enter a question, and get a list of similar tweets from the database.",
+        button="Submit",  # Add a submit button
+    )
+    demo.launch()
+else:
 
 
-demo = gr.Interface(
-    fn=search,
-    inputs=[
-        gr.Textbox(lines=1, placeholder="Keyword"),
-        n_tweet_component,
-        minimum_lenght,
-        gr.Textbox(lines=1, placeholder="Rock, Metal, EDM, Dance, Heavy Metal, Techno, Funky, Grunge, Pop, Kpop, Rap"),
-    ],
-    outputs=gr.Textbox(placeholder="List of Tweets"),
-    live=False,  # Disable live updates
-    title="Grapho Search",
-    description="Enter a question, and get a list of similar tweets from the database.",
-    button="Submit",  # Add a submit button
-)
+    with gr.Blocks() as demo:
+        # title
+        gr.Markdown("# Grapho Search\n"
+                   "Enter a question, and get a list of similar tweets from the database.")
+        with gr.Row():
+            with gr.Column():
+                keywords_box =  gr.Textbox(value="Music", lines=1, label="Keywords")
+                n_tweet_component =  gr.Number(value=100, default=20, label="Number of Tweets")
+                minimum_lenght = gr.Number(value=120, default=120, label="Minimum number of characters")
+                topics_box =  gr.Textbox(
+                    lines=1,
+                    label="List of Topics",
+                    value="Rock, Metal, EDM, Dance, Heavy Metal, Techno, Funky, Grunge, Pop, Kpop, Rap"
+                    )
+                with gr.Row():
+                    clear_button = gr.ClearButton([
+                    keywords_box,
+                    n_tweet_component,
+                    minimum_lenght,
+                    topics_box
+                    ])
+                    search_button : gr.Button = gr.Button("Search", variant="primary")
+            with gr.Column():
+                tweets = gr.Textbox(placeholder="List of Tweets")
+        with gr.Row():
+            with gr.Column():
+                dropdown_topic = gr.Textbox(lines=1, placeholder="Keyword")
+                plot_button = gr.Button("Plot", variant="primary")
+            with gr.Column():
+                plot_image = gr.Image(type="pil")
 
-demo.launch()
+        search_button.click(
+            search,
+            inputs=[
+                keywords_box,
+                n_tweet_component,
+                minimum_lenght,
+                topics_box
+            ],
+            outputs=[
+                tweets
+            ])
+
+        plot_button.click(
+            generate_image,
+            inputs=dropdown_topic,
+            outputs=plot_image
+        )
+        # react to clicks on search
+
+    demo.launch()
